ec4d4fec7d11d68231161bb7a30711f8e6745a1c
Jacob House <jacob@jacobhouse.ca>
2019-01-15 19:44:19 -0330

    Added docs dummy page

---
+++ docs/precache-manifest.2f038f3a648a5f530085aad8e70e08e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c322fdf6fb79fb86a26",
+    "url": "/librarian/static/css/main.6456714d.chunk.css"
+  },
+  {
+    "revision": "6c322fdf6fb79fb86a26",
+    "url": "/librarian/static/js/main.6c322fdf.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/librarian/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2d96d9bb5c2266819eeb",
+    "url": "/librarian/static/js/runtime~main.2d96d9bb.js"
+  },
+  {
+    "revision": "6f9dfdaa47fb803f96ab35cce983291b",
+    "url": "/librarian/static/media/logo.6f9dfdaa.svg"
+  },
+  {
+    "revision": "87643291531d42e01cb38e8491a0337f",
+    "url": "/librarian/index.html"
+  }
+];
